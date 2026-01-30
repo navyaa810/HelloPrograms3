f927ba148e0eaeabd2102a9264b1f08682938d60
navyaa810 <thisisnavyasiddapuram08@gmail.com>
2026-01-30 14:48:29 +0530

    Create helloserver.js

---
+++ helloserver.js
@@ -0,0 +1,12 @@
+// helloserver.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!\n');
+});
+
+server.listen(3000, () => {
+  console.log('Hello server running at http://localhost:3000');
+});
